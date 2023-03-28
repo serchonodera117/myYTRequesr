88f549f325d35db01ec0501d861bf2c6b3781c5d
serchonodera117 <62153337+serchonodera117@users.noreply.github.com>
2023-03-27 20:23:24 -0600

    Update Form1.cs

--- myYTRequest_f/Form1.cs
+++ myYTRequest_f/Form1.cs
@@ -33,7 +33,7 @@ namespace myYTRequest_f
 
             yt = new YouTubeService(new BaseClientService.Initializer()
             {
-                ApiKey = "AIzaSyCca9kwDhBgJ8SAz1zAAuYSsv-3KIZatQk"
+                ApiKey = "Your API token" //to get it you have to log in Google developer console 
             });
         }
 
